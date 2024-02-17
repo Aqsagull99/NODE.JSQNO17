bb949104eec0bdedc7a24ca5b1ec53726fe6c36c
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 09:15:14 +0500

    Add files via upload

---
+++ qno17.js
@@ -0,0 +1,15 @@
+//  QNO:17
+//  Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
+// • Start with your program from Exercise 16. Add a new line that prints a message saying that you can invite only two people for dinner.
+// • Remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+// • Print a message to each of the two people still on your list, letting them know they’re still invited.
+// • Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+// let guestlist=["saba","noreen","naila","musfira","mehru", "muntaha"]
+// console.log("i can invited only two people  to dinner");
+// while(guestlist.length>2){
+// let removeguest=guestlist.pop()
+// console.log(`dear ${removeguest}, you are not invited to the dinner`)
+// }
+// guestlist.map((item)=> console.log(`Dear ${item}, you are invited to the dinner `));
+// guestlist.pop()
+// console.log(guestlist)
